4b0435c4c3f67ccded92d8d90ee2a27435bb34f6
Evgeniy Voronov <108752375+evgenfromgus@users.noreply.github.com>
2025-01-17 18:31:39 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,11 @@
+PROMOCODE_1 = "GIVEMEHALYAVA"
+PROMOCODE_2 = "DC120"
+customer_data = {
+    "first_name": "Иван",
+    "last_name": "Иванов",
+    "address": "Улица Пушкина, дом 12",
+    "city": "Москва",
+    "state": "Москва",
+    "postcode": "101000",
+    "phone": "+7 123 456-78-90",
+}
